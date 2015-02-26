0b3c4495058c217011eaa614e8acc6c5e4c9eea0
grosa82 <grosa82@gmail.com>
2015-02-26 14:14:56 -0700

    Clean build.gradle

--- app/src/main/java/com/cs246/bakery/myapplication/User.java
+++ app/src/main/java/com/cs246/bakery/myapplication/User.java
@@ -22,6 +22,47 @@ public class User {
      * Email address
      */
     public String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     /**
      * User full name
      */
