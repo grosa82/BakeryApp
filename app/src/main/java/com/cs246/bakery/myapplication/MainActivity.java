a78b7a947ef2ce395e84f1fdbe5a04e88ba023b1
grosa82 <grosa82@gmail.com>
2015-03-05 21:25:44 -0700

    Add new menu items to test the app

--- app/src/main/java/com/cs246/bakery/myapplication/MainActivity.java
+++ app/src/main/java/com/cs246/bakery/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.cs246.bakery.myapplication.model.Helper;
@@ -38,17 +39,17 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_login:
+                startActivity(new Intent(MainActivity.this, signOn.class));
+                return true;
+            case R.id.action_create:
+                startActivity(new Intent(MainActivity.this, UserForm.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void signIn(View view) {
