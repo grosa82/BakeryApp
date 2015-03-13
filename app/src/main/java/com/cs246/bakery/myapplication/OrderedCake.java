7d128afe2fd6db9e2e61da807873a3dc906c17bf
grosa82 <grosa82@gmail.com>
2015-03-13 12:05:00 -0700

    Merge solved

--- app/src/main/java/com/cs246/bakery/myapplication/OrderedCake.java
+++ app/src/main/java/com/cs246/bakery/myapplication/OrderedCake.java
@@ -24,7 +24,6 @@ public class OrderedCake extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_ordered_cake, menu);
         return true;
     }
 
